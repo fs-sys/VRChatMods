3ced0ac8d1ec17ecb8ed695a3c219acbbaf3d969
FS <47338369+TCOSFS@users.noreply.github.com>
2022-07-13 04:32:01 -0500

    Bump Version

--- ClassicPlates/ClassicPlates.cs
+++ ClassicPlates/ClassicPlates.cs
@@ -4,7 +4,7 @@ using ClassicPlates.Patching;
 using MelonLoader;
 using VRC;
 
-[assembly: MelonInfo(typeof(ClassicPlates.ClassicPlates), "ClassicPlates", "1.0.4", ".FS.#8519")]
+[assembly: MelonInfo(typeof(ClassicPlates.ClassicPlates), "ClassicPlates", "1.0.5", ".FS.#8519")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonOptionalDependencies("UIExpansionKit")]
 namespace ClassicPlates;
